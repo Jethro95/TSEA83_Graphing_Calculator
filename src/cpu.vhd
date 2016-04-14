79275d90c6bfe97ff1a075091dca10602d2cac9a
Felix Härnström <felha423@student.liu.se>
2016-04-14 14:55:47 +0200

    ASR/ASL now properly update flags X, N, Z

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -353,19 +353,35 @@ begin
                 lengthhack_result := lengthhack_result + unsigned(to_signed(lengthhack_float, 32));
                 AR <= signed(lengthhack_result);
             elsif (ALU = "01001") then -- ASR 
-                flag_C <= AR(0);
-                if (DATA_BUS = 0) then
+                if (MM = "00") and (to_integer(DATA_BUS) = 0 ) then
+                    flag_C <= AR(0);
+                    flag_X <= AR(0);
                     AR <= SHIFT_RIGHT(signed(AR),1);
                 else
+                    if(to_integer(DATA_BUS) != 0) then
+                        -- C and X unaffected by a shift count of zero
+                        flag_C <= AR(to_integer(DATA_BUS) - 1);
+                        flag_X <= AR(to_integer(DATA_BUS) - 1);
+                    end if;
                     AR <= SHIFT_RIGHT(signed(AR),to_integer(DATA_BUS));
                 end if;
+                if (AR = 0) then flag_Z <= '1'; else flag_Z <= '0'; end if;
+                flag_N <= AR(31);
             elsif (ALU = "01010") then -- ASL
-                flag_C <= AR(31);
-                if (DATA_BUS = 0) then
+                if (MM = "00") and (to_integer(DATA_BUS) = 0 ) then
+                    flag_C <= AR(31);
+                    flag_X <= AR(31);
                     AR <= SHIFT_LEFT(signed(AR),1);
                 else
+                    if(to_integer(DATA_BUS) != 0) then
+                        -- C and X unaffected by a shift count of zero
+                        flag_C <= AR(32 - to_integer(DATA_BUS));
+                        flag_X <= AR(32 - to_integer(DATA_BUS));
+                    end if;
                     AR <= SHIFT_LEFT(signed(AR),to_integer(DATA_BUS));
                 end if;
+                if (AR = 0) then flag_Z <= '1'; else flag_Z <= '0'; end if;
+                flag_N <= AR(31);
            end if;
         end if;
     end process;
