8f0087574a97499b6721c968328754e831443d0e
Silas Lenz <silas.lenz@gmail.com>
2016-05-02 23:21:06 +0200

    ComileAndLoad now updates the limit for when PM will be loaded

--- sillescript2/compileAndLoad.py
+++ sillescript2/compileAndLoad.py
@@ -9,15 +9,22 @@ import compiler
 
 START_OF_P_MEM = "-- program Memory"
 END_OF_P_MEM = "signal p_mem : p_mem_t := p_mem_c;"
+START_OF_PM_LIMITER = "when ASR <"
+END_OF_PM_LIMITER = "else (others =>"
 TARGET_FILE = "../src/cpu.vhd"
 
-def loadTo(newCode, targetFilename):
+def loadTo(newCode, linecount, targetFilename):
     contents = ""
     with open(targetFilename) as f:
         contents = f.read()
     preCode, rest = contents.split(START_OF_P_MEM)
     code, postCode = rest.split(END_OF_P_MEM)
     newfile = preCode + START_OF_P_MEM + "\n" + newCode + "\n\n" + END_OF_P_MEM  + postCode
+
+    # Also limit for which ASR PM is set.
+    preCode, rest = newfile.split(START_OF_PM_LIMITER)
+    code, postCode = rest.split(END_OF_PM_LIMITER)
+    newfile = preCode + START_OF_PM_LIMITER + str(linecount) +" " + END_OF_PM_LIMITER  + postCode
     with open(targetFilename, "w") as f:
         f.write(newfile)
 
@@ -27,7 +34,7 @@ def main():
         print("Stopped due to build errors.")
         return
     result = compiler.fancifyForVHDL(buildResult)
-    loadTo(result, TARGET_FILE)
+    loadTo(result, len(buildResult), TARGET_FILE)
 
 if __name__ == '__main__':
     main()
